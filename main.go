580006f5e1052021879cf6b2e6a53f116f86ff36
ia <isaac.ardis@gmail.com>
2018-06-17 00:57:39 +0200

    all: gofmt
    Run standard gofmt command on project root.
    
    - go version go1.10.3 darwin/amd64
    
    Signed-off-by: ia <isaac.ardis@gmail.com>

--- main.go
+++ main.go
@@ -5,70 +5,70 @@ import (
 	"math/rand"
 )
 
-func randomFillin(arr []int){
-	for index := range arr{
-		arr[index] = rand.Int()%100
+func randomFillin(arr []int) {
+	for index := range arr {
+		arr[index] = rand.Int() % 100
 	}
 }
 
-func findPos(val int, arr []int) int{
+func findPos(val int, arr []int) int {
 	i := 0
-	for val < arr[i]{
+	for val < arr[i] {
 		i++
 	}
 	return i
 }
 
-func insert(val int, arr []int, length *int){
+func insert(val int, arr []int, length *int) {
 	size := *length
 	pos := findPos(val, arr)
-	for i:=0; i<(size - pos);i++{
-		arr[size - i] = arr[size - i - 1]
+	for i := 0; i < (size - pos); i++ {
+		arr[size-i] = arr[size-i-1]
 	}
 	arr[pos] = val
 	*length++
 }
 
-func sort(arr []int) []int{
+func sort(arr []int) []int {
 	result := make([]int, cap(arr), cap(arr))
 	length := 0
-	for _, val := range arr{
+	for _, val := range arr {
 		insert(val, result, &length)
 	}
 	return result
 }
 
-func swap(arr []int, posA int, posB int){
+func swap(arr []int, posA int, posB int) {
 	tmp := arr[posA]
 	arr[posA] = arr[posB]
 	arr[posB] = tmp
 }
 
-func update(arr []int, pos int, val int){
+func update(arr []int, pos int, val int) {
 	origin := arr[pos]
 	arr[pos] = val
-	if (val > origin){
-		for i:=pos-1;i>=0;i--{
-			if val > arr[i]{
+	if val > origin {
+		for i := pos - 1; i >= 0; i-- {
+			if val > arr[i] {
 				swap(arr, i, pos)
 				pos = i
-			}else{
+			} else {
 				break
 			}
 		}
-	}else if (val < origin){
-		for i:=pos+1;i<=len(arr);i++{
-			if val < arr[i]{
+	} else if val < origin {
+		for i := pos + 1; i <= len(arr); i++ {
+			if val < arr[i] {
 				swap(arr, i, pos)
 				pos = i
-			}else{
+			} else {
 				break
 			}
 		}
 	}
 }
 
-func main(){
+func main() {
 	a := make([]int, 10)
 	randomFillin(a)
 	b := sort(a)
